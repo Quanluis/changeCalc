900c6fe0e2fdb23969addf4287a0bdfddafc1acf
Luis Garzon <quanluis@protonmail.com>
2021-10-11 18:53:19 -0400

    Added the logic for the website, will work next on dislaying the results on the input box.

--- calculate.js
+++ calculate.js
@@ -1,5 +1,36 @@
 "use strict";
 
+// Variables 
+
+let input = document.getElementById("cents");
+
+let result;
+let quarters = 25;
+let dimes = 10;
+let nickles = 5;
+let pennies = 1;
+
+document.getElementById("calculate").onclick = () => {
+
+
+    if(isNaN(input.value) == true){
+
+
+        alert('Enter a number.')
+
+    } else{
+        
+        console.log('How many Quarters fit: ' + Math.floor(result));
+
+        console.log('How many dimes fit ' + Math.floor((input.value % quarters) / dimes));
+
+        console.log('How many nickles fit: ' + Math.floor((input.value % quarters % dimes) / nickles));
+
+        console.log('How many pennies fit: '+ Math.floor((input.value % quarters % dimes % nickles) / pennies));
+        
+    }
+}
+
 $(document).ready( () => {
     $("#calculate").click( () => {
         
